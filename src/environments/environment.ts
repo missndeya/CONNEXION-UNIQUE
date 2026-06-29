4c6f4ef04f9cf012cae05759f9ad073e46a67ab2
missndeya <ndeyethiamastou@gmail.com>
2026-06-29 14:58:35 +0000

    Ajout des fonctions COF, AGO et OBS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,32 @@
 export const environment = {
   production: false,
+
+
+  /*PARTIE EXECUTION*/
+  /*   dashboardElaborationUrl: 'http://localhost:62661',
+    authUrl: 'http://localhost:4200/login',
+    backElaborationUrl: 'http://localhost:8085/api',
+    backExecutionUrl: 'http://localhost:8012',
+    loginUrl: 'http://localhost:8093/auth/login',
+    dashboardExecutionUrl: 'http://localhost:62661',
+    baseUrl: 'http://localhost:8093' */
+
+
   // loginUrl: 'https://dev1.dgf.sn:8444/sysbudgep-authentification/auth/login',
   //private readonly loginUrl = '/proxy/sysbudgep-authentification/auth/login';
-  loginUrl: 'http://localhost:8093/auth/login',
-  dashboardExecutionUrl: 'http://localhost:62661',
- // dashboardElaborationUrl: 'http://localhost:50317',
-  dashboardElaborationUrl: 'http://localhost:62661',
+  // dashboardElaborationUrl: 'http://localhost:50317',
+  // baseUrl : 'https://dev1.dgf.sn:8444/sysbudgep-authentification'
+
+
+
+  /*PARTIE ELABORATION*/
+  loginUrl: 'http://localhost:8012/auth/login',
+  //loginUrl: 'https://dev1.dgf.sn:8444/sysbudgep-authentification/auth/login',
+  dashboardExecutionUrl: 'https://dev1.dgf.sn',
+  dashboardElaborationUrl: 'http://localhost:50317',
   authUrl: 'http://localhost:4200/login',
   backElaborationUrl: 'http://localhost:8085/api',
   backExecutionUrl: 'http://localhost:8012',
- // baseUrl : 'https://dev1.dgf.sn:8444/sysbudgep-authentification'
-  baseUrl : 'http://localhost:8093'
+  // baseUrl : 'https://dev1.dgf.sn:8444/sysbudgep-authentification'
+  baseUrl: 'http://localhost:8012'
 };
